356d433ccaa4bc6944963f5a0e8da891dd00fc51
Yashkant103 <100015305+Yashkant103@users.noreply.github.com>
2022-12-28 20:30:22 +0530

    Uploaded Lab-02

---
+++ Lab-02/max_outOf_three.sh
@@ -0,0 +1,28 @@
+echo -e "/t/t!Find Maximum Number out of Three NUmbers!"
+echo "Enter Number"
+read a
+echo "Enter other Number"
+read b
+echo "Enter other Number"
+read c
+
+echo -e "\n"
+
+if [ $a -gt $b ]
+then
+
+    if [ $a -gt $c ]
+    then
+    echo "$a is maximum Number."
+
+    else
+    echo "$c is maximum Number."
+    fi
+
+elif [ $b -gt $a ]
+then
+echo "$b is maximum Number."
+
+else
+echo "$c is maximum Number."
+fi
